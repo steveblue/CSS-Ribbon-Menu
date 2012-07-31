21b52a4f26418e371ae5132f40eea7a678504c32
Steve Belovarich <steve@installationspace.com>
2012-07-30 22:31:01 -0700

    Updated JS to reflect codepen.io, README.md now includes actual information, and CSS comments now are in README.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-README for CSS Ribbon Menu
+Pure CSS3 Ribbon Menu 
+http://stephenbelovarich.com
+v1.2b1 | 201207
+NOTE:WORK IN PROGRESS
+USE WITH CAUTION, ADAPT TO YOUR LAYOUT
+
+This ribbon menu is different than most. It includes a cool 3D hover effect that can also be made :active, making it look pressed down. 
+
+
+v 1.1 
+
+fixed FF13 and iOS bug that dropped .navafter below the ribbon, good for keeping the 3D illusion.
+
+removed box-shadow from nav ul, was making a hairline horizontal impression on the top of each pressed down nav element.
+
+added gradients to .navbefore and .navafter to emphasize their depth against the background cut out ends
+
+removed reset, b/c codepen includes it now
+
+v 1.0
+
+CSS3 Transforms! The buttons are just begging to be animated.
+
+CSS3 Gradients! Hover over a button to see what I mean.
+
+Depth while hovering is created using pseudo elements, transform and gradients.
+
+When the nav is directed to content on a single page layout on iPad the default behavior keeps touched buttons pressed down. You could make an active pseudo class to keep the active button pressed when the user is viewing content.
+
+Lots of pseudo elements! Whoo hoo! Which means older versions of IE can't handle it. But then again, it says CSS3 Ribbon Menu
+
+Eric Meyer's RESET used, just b/c its always in my CSS so included it here to make sure everything works. *REMOVED in v1.1
+
+Well commented so you can learn how it was made.
+
--- js/ribbon.menu.v1.js
+++ js/ribbon.menu.v1.js
@@ -1,11 +1,41 @@
 $(document).ready(function () {
 
-$('nav ul span.navafter').css('width', '280px');
+/*
+Changes the width of .navafter (the ribbon after the nav buttons) when the last-child li is pressed. I chose to use eq method instead 
+of last-child b/c it's a more reliable selector.
+
+If this wasn't corrected, the nav menu would overflow. Change the width and eq() accordingly when you add and subtract buttons. This also can be down programmatically, but loads slow. 
+
+Currently bugs out if you hover over the :before skewed depth of the last-child li element. Not a big deal, but kinda annoying.
+*/
+
+var deviceAgent = navigator.userAgent.toLowerCase();
+var initWidth, hoverWidth, fixRight; 
+
+if ($.browser.mozilla && $.browser.version >= "1.8") { 
+  initWidth = 277;
+	hoverWidth = 259; 
+  fixRight = 1;
+  }
+else if (deviceAgent.match(/(iphone|ipod|ipad)/)){
+ 	initWidth = 277;
+	hoverWidth = 259; 
+  fixRight = 1;
+  }
+else {
+  initWidth = 280;
+	hoverWidth = 262;  
+  fixRight = 0;
+}
+
+$('nav ul span.navafter').css('width', initWidth + 'px');
+$('nav ul span.navafter').css('right', fixRight + 'px');
 $("nav ul li").eq(5).children('a:link').hover(function () {
-              $('nav ul span.navafter').css('width', '262px');
+              $('nav ul span.navafter').css('width', hoverWidth + 'px');
             }, function () {
-               $('nav ul span.navafter').delay(3000).css('width', '280px');
+               $('nav ul span.navafter').css('width', initWidth + 'px');
   });
+
   
   
 });
